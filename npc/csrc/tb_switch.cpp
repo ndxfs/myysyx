cc14c442ac57710d72eb6b1a7e0f130eee146bcf
tracer-ysyx <tracer@ysyx.org>
2025-08-05 19:29:04 +0800

    > sim RTL ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:29:04 up  5:28,  1 user,  load average: 0.12, 0.04, 0.01

--- npc/csrc/tb_switch.cpp
+++ npc/csrc/tb_switch.cpp
@@ -2,32 +2,36 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "Vswitch.h"
-#include "verilated.h"
-#include "verilated_vcd_c.h"
+#include <nvboard.h>
+//#include "verilated.h"
+//#include "verilated_fst_c.h"
 
-#define MAX_SIM_TIME 20
-vluint64_t sim_time = 0;
+//#define MAX_SIM_TIME 20
+//vluint64_t sim_time = 0;
+static Vswitch top;
+void nvboard_bind_all_pins(Vswitch* top);
 
 int main(int argc, char** argv)
 {
-	Vswitch* top = new Vswitch;
-	Verilated::traceEverOn(true);
-	VerilatedVcdC *m_trace = new VerilatedVcdC;
-	top->trace(m_trace, 5);
-	m_trace->open("waveform.vcd");
-	while (sim_time < MAX_SIM_TIME)
+	nvboard_bind_all_pins(&top);
+	nvboard_init();
+	//Verilated::traceEverOn(true);
+	//VerilatedFstC *m_trace = new VerilatedFstC;
+	//VerilatedVcdC *m_trace = new VerilatedVcdC;
+	//top->trace(m_trace, 5);
+	//m_trace->open("waveform.fst");
+	//while (sim_time < MAX_SIM_TIME)
+	while(1)
 	{
-		int a = rand() & 1;
-		int b = rand() & 1;
-		top->a = a;
-		top->b = b;
-		top->eval();
-		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		assert(top->f == (a ^ b));
-		m_trace->dump(sim_time);
-		sim_time++;
+		//int a = rand() & 1;
+		//int b = rand() & 1;
+		top.eval();
+		//printf("a = %d, b = %d, f = %d\n", a, b, top.f);
+		//assert(top.f == (a ^ b));
+		nvboard_update();
+		//m_trace->dump(sim_time);
+		//sim_time++;
 	}
-	m_trace->close();
-	delete top;
+	//m_trace->close();
   exit(EXIT_SUCCESS);
 }
