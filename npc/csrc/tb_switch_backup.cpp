eef43a1d9eef4799d8214fc5e3adda2a4d8f06ab
hyx <2086244159@qq.com>
2025-08-05 19:50:26 +0800

    接入NVBoard，实现流水灯

---
+++ npc/csrc/tb_switch_backup.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vswitch.h"
+#include "verilated.h"
+#include "verilated_fst_c.h"
+
+#define MAX_SIM_TIME 20
+vluint64_t sim_time = 0;
+
+int main(int argc, char** argv)
+{
+	Vswitch* top = new Vswitch;
+	Verilated::traceEverOn(true);
+	VerilatedFstC *m_trace = new VerilatedFstC;
+	//VerilatedVcdC *m_trace = new VerilatedVcdC;
+	top->trace(m_trace, 5);
+	m_trace->open("waveform.fst");
+	while (sim_time < MAX_SIM_TIME)
+	{
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		assert(top->f == (a ^ b));
+		m_trace->dump(sim_time);
+		sim_time++;
+	}
+	m_trace->close();
+	delete top;
+  exit(EXIT_SUCCESS);
+}
