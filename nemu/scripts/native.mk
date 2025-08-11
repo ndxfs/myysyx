52fb052dd4474483ca79fe57f0c974ffadbdae10
tracer-ysyx <tracer@ysyx.org>
2025-08-11 20:36:04 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:04 up  5:32,  1 user,  load average: 0.03, 0.03, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -38,8 +38,9 @@ run: run-env
 	$(NEMU_EXEC)
 
 gdb: run-env
-	$(call git_commit, "gdb NEMU")
+	#$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
+	@echo $(BINARY) $(NEMU_EXEC)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
