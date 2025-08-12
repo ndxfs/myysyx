ce9003542fb9fed086d9228f6873c18d0a643a17
tracer-ysyx <tracer@ysyx.org>
2025-08-12 12:41:02 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:41:02 up  3:11,  1 user,  load average: 0.01, 0.03, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -94,8 +94,8 @@ static int cmd_x(char *args) {
 			uint32_t addr = (uint32_t)addr_long;
 			for(int i = 0; i < N; i++ )
 			{
-				addr_read = vaddr_read(addr + 4 * i, 4);
-				printf("0x%08x  %08x\n", addr, addr_read);
+				addr_read = vaddr_read(addr+4*i, 4);
+				printf("0x%08x:  0x%08x\n", addr+4*i, addr_read);
 				//printf("%-6s:  0x%08x  %u\n", regs[i], gpr(i), gpr(i));
 			}
 		}
