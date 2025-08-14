d8fa0da088c345bc657e64d023e868e4f6315f96
tracer-ysyx <tracer@ysyx.org>
2025-08-14 18:52:30 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:52:30 up 18 min,  1 user,  load average: 0.14, 0.25, 0.22

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -204,7 +204,7 @@ static int cmd_pt(char *args) {
     } 
     else
     {
-      printf("\033[32mline %d fail, expect %u, result %u\033[0m\n", i, expected, result);
+      printf("\033[31mline %d fail, expect %u, result %u\033[0m\n", i, expected, result);
     }
   }
   fclose(fp);
