ad6f3769a90200dba5b79327515a222c62033f12
tracer-ysyx <tracer@ysyx.org>
2025-08-14 10:47:39 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:39 up 24 min,  1 user,  load average: 0.12, 0.17, 0.14

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -167,7 +167,7 @@ static int cmd_d(char *args) {
 
 static int cmd_pt(char *args) {
   bool success;
-  FILE *fp = fopen("~/ysyx/ysyx-workbench/nemu/tools/gen-expr/input", "r");
+  FILE *fp = fopen("../tools/gen-expr/input", "r");
   char result_buf[64] = {};
   char expr_buf[65536] = {};
   if (!fp) {
