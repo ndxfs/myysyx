8f6f709eb097b7f45f9e3d4aba54224ea275963f
tracer-ysyx <tracer@ysyx.org>
2025-08-14 22:46:16 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:16 up  4:12,  1 user,  load average: 0.17, 0.24, 0.12

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,7 +19,7 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
-
+#include <unistd.h>
 // this should be enough
 static char buf[65536] = {};
 static char *buf_ptr = buf;
@@ -84,6 +84,10 @@ static void gen_rand_expr() {
     }
 }
 
+int has_overflow_warning(const char *compile_output) {
+  return strstr(compile_output, "warning: integer overflow") != NULL;
+}
+
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
@@ -106,17 +110,95 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    // int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    // if (ret != 0) continue;
+
+    FILE *compile_fp = popen("gcc /tmp/.code.c -o /tmp/.expr 2>&1", "r");
+    assert(compile_fp != NULL);
+
+    char compile_output[8192] = {};
+    //size_t bytes_read = fread(compile_output, 1, sizeof(compile_output) - 1, compile_fp);
+    pclose(compile_fp);
+
+    // 检查是否有溢出警告
+    if (has_overflow_warning(compile_output)) {
+      continue; // 有警告，重新生成
+    }
+
+    // 检查编译是否成功
+    if (access("/tmp/.expr", F_OK) == -1) {
+      continue; // 编译失败，重新生成
+    }
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
+    
+
     int result;
-    ret = fscanf(fp, "%d", &result);
+    int ret = fscanf(fp, "%d", &result);
+
     pclose(fp);
 
-    printf("%u\n%s\n", result, buf);
+    if (ret == 1) {
+      printf("%u\n%s\n", result, buf);
+      continue;
+    }
   }
   return 0;
 }
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <assert.h>
+// #include <string.h>
+// 
+// // this should be enough
+// static char buf[65536] = {};
+// static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+// static char *code_format =
+// "#include <stdio.h>\n"
+// "int main() { "
+// "  unsigned result = %s; "
+// "  printf(\"%%u\", result); "
+// "  return 0; "
+// "}";
+// 
+// static void gen_rand_expr() {
+//   buf[0] = '\0';
+// }
+// 
+// int main(int argc, char *argv[]) {
+//   int seed = time(0);
+//   srand(seed);
+//   int loop = 1;
+//   if (argc > 1) {
+//     sscanf(argv[1], "%d", &loop);
+//   }
+//   int i;
+//   for (i = 0; i < loop; i ++) {
+//     gen_rand_expr();
+// 
+//     sprintf(code_buf, code_format, buf);
+// 
+//     FILE *fp = fopen("/tmp/.code.c", "w");
+//     assert(fp != NULL);
+//     fputs(code_buf, fp);
+//     fclose(fp);
+// 
+//     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+//     if (ret != 0) continue;
+// 
+//     fp = popen("/tmp/.expr", "r");
+//     assert(fp != NULL);
+// 
+//     int result;
+//     ret = fscanf(fp, "%d", &result);
+//     pclose(fp);
+// 
+//     printf("%u %s\n", result, buf);
+//   }
+//   return 0;
+// }
+
