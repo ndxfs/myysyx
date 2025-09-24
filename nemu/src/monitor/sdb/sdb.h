b610c90f129bde968c8283b67080f72f88198468
tracer-ysyx <tracer@ysyx.org>
2025-09-24 10:08:45 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:08:45 up  1:12,  1 user,  load average: 1.02, 0.35, 0.17

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -18,6 +18,16 @@
 
 #include <common.h>
 
+typedef struct watchpoint {
+  int NO;
+  struct watchpoint *next;
+
+  /* TODO: Add more members if necessary */
+
+} WP;
+
 word_t expr(char *e, bool *success);
+WP* new_wp();
+void free_wp(WP *wp);
 
 #endif
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -17,13 +17,7 @@
 
 #define NR_WP 32
 
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
 
-  /* TODO: Add more members if necessary */
-
-} WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
@@ -40,4 +34,49 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+static int total_NO=0;
+WP* new_wp()
+{
+	//TODO:NO更新
+	if(free_ == NULL)
+	{
+		printf("No empty node to store watchpoint\n");
+		assert(0);
+	}
+	else
+	{
+		WP* new_node = free_;
+		new_node -> next = head;
+		head = new_node;
+		head -> NO = total_NO;
+		total_NO ++;
+		free_ = free_ -> next;
+		return head;
+	}
+}
+void free_wp(WP *wp)
+{
+	WP* prev_WP = NULL;
+	bool find_flag = false;
+	WP *current_WP = head;
+	while(current_WP != NULL)
+	{
+		if(current_WP == wp)
+		{
+			if(current_WP != head) prev_WP -> next = current_WP -> next;
+			else head = head -> next;
+			current_WP -> next = free_;
+			free_ = current_WP;
+			find_flag = true;
+			break;
+		}
+		prev_WP = current_WP;
+		current_WP = current_WP -> next;
+	}
 
+	if(find_flag == false)
+	{
+		printf("Not find NO:%d\n", wp -> NO);
+		assert(0);
+	}
+}
