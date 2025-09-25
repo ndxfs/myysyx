004fbcb3878010609c2202d387982311c0f57bb4
tracer-ysyx <tracer@ysyx.org>
2025-09-25 12:27:01 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:27:01 up  4:23,  1 user,  load average: 0.19, 0.07, 0.01

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -43,6 +43,12 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 			return gpr(i);
 		}
 	}
+
+  if(strcmp("pc", s) == 0)
+  {
+    *success = true;
+    return cpu.pc;
+  }
   return 0;
 }
 
