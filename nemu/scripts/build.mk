e25aea7cdf1a8e5a5c92302a6e7db48aabbd63e1
tracer-ysyx <tracer@ysyx.org>
2025-09-28 22:46:30 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:30 up 11:45,  1 user,  load average: 0.17, 0.06, 0.01

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -60,7 +60,6 @@ count_lines:
 	@echo "Total lines without empty of .c and .h files:"
 	@find . \( -name "*.c" -o -name "*.h" \) -exec sh -c '
 	  for file do
-	    # 单个文件：读内容→过滤空行→统计非空行（避免因文件空导致输出0）
 	    cat "$file" | grep -v "^$" | wc -l
 	  done
 	' sh {} + | awk '{sum += $1} END {print sum}'  # 汇总非空行总数
