84813288d6fb3244849034692f4f4c94f749052d
tracer-ysyx <tracer@ysyx.org>
2025-09-28 22:43:43 +0800

    > compile NEMU ysyx_22040000 何逸轩 Linux ubuntu-virtual-machine 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:43:43 up 11:43,  1 user,  load average: 0.10, 0.03, 0.00

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -55,8 +55,14 @@ $(BINARY):: $(OBJS) $(ARCHIVES)
 
 #统计代码行数
 count_lines:
-#@echo "Total lines of .c and .h files:"
-	find . \( -name "*.c" -o -name "*.h" \) -exec wc -l {} + | grep "total"
+	@echo "Total lines of .c and .h files:"
+	@find . \( -name "*.c" -o -name "*.h" \) -exec wc -l {} + | grep "total"
+	@echo "Total lines without empty of .c and .h files:"
+	@find . \( -name "*.c" -o -name "*.h" \) -exec sh -c '
+	  for file do
+	    cat "$file" | grep -v "^$" | wc -l  # 单个文件：读内容→滤空行→统计非空行
+	  done
+	' sh {} + | awk '{sum += $1} END {print "Total non-empty lines: " sum}' | grep -s .
 
 clean:
 	-rm -rf $(BUILD_DIR)
